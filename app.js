5945bd6374f98e3c6b676f18060ecff5d0ec3734
Ifyanyit <ngeneifyanyi@gamail.com>
2023-04-11 23:40:59 +0100

    imported required modules

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy; // used to set up google auth
+const findOrCreate = require("mongoose-findorcreate"); // used with the google auth. find or create a user google id
